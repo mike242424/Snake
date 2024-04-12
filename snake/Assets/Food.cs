88db9263a4548b106a194bc55f3735d2adb9199f
Michael Byers <mikebyers24@gmail.com>
2024-04-12 10:47:22 -0400

    added function to randomize food component at start of game

--- snake/Assets/Food.cs
+++ snake/Assets/Food.cs
@@ -1,8 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEngine;
 
 public class Food : MonoBehaviour
 {
+    public BoxCollider2D gridArea;
 
+    void Start()
+    {
+        RandomizePostion();
+    }
+
+    void RandomizePostion()
+    {
+        Bounds bounds = gridArea.bounds;
+
+        float x = Random.Range(bounds.min.x, bounds.max.x);
+        float y = Random.Range(bounds.min.y, bounds.max.y);
+
+        transform.position = new UnityEngine.Vector3(Mathf.Round(x), Mathf.Round(y), 0.0f);
+    }
 }
